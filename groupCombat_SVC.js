2637e43cd4e08aaa5db43e486f4679b694713d2e
Justin Lane <justin.l.lane@gmail.com>
2012-11-08 11:06:44 -0700

    Moved effects and actions to NPC combat UI

--- groupCombat_SVC.js
+++ groupCombat_SVC.js
@@ -12,30 +12,19 @@ GM.groupCombatSVC = function(parent) {
 	this.popup.setOverlayClass("fog");
 
 	this.ui = this.popup.addPanel("Combat");
-
-	var p = this.ui.addPanel("Actions");
-	var b = p.addButton("Attack",new db.link(this,this.combatPopup,[]));
-	var b = p.addButton("Defend",new db.link(this,this.defensiveAction,[]));
-	var b = p.addButton("Stun",new db.link(this,this.addEffect,["stun",1]));
-	var b = p.addButton("Grapple");
-	var b = p.addButton("Prone",new db.link(this,this.addEffect,["prone",1]));
-	var b = p.addButton("K.O.");
-	var b = p.addButton("Bound/Helpless");
-	var b = p.addButton("Fight Defensively");
-	var b = p.addButton("Throwing Caution");
-	var b = p.addButton("Delay");
-	var b = p.addButton("Rush");
-	var b = p.addButton("Reset",new db.link(this,this.resetEffects,["all"]));
-
-	var p = this.ui.addPanel("Effects");
-	this.panels.effects = p;
-	this.refreshEffects();
 };
 
 // -------------------------------------------------------------------------------------------------
 // initialize
 // -------------------------------------------------------------------------------------------------
 GM.groupCombatSVC.prototype.initialize = function() {
+	this.parent.appendChild(this.popup);
+};
+
+// -------------------------------------------------------------------------------------------------
+// refreshView
+// -------------------------------------------------------------------------------------------------
+GM.groupCombatSVC.prototype.refreshView = function() {
 };
 
 // -------------------------------------------------------------------------------------------------
