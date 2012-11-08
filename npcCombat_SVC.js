3a41c2a1cd4cada88e551e6a9b1ee77bce0be54a
Justin Lane <justin.l.lane@gmail.com>
2012-11-08 11:08:01 -0700

    Funky commit...

--- npcCombat_SVC.js
+++ npcCombat_SVC.js
@@ -2,6 +2,24 @@ GM.npcCombatSVC = funciton(parent) {
 	this.parent = parent;
 	this.mainframe = new lib.mainframe(parent.mainframe);
 	this.npc = null;
+
+	var p = this.ui.addPanel("Actions");
+	var b = p.addButton("Attack",new db.link(this,this.combatPopup,[]));
+	var b = p.addButton("Defend",new db.link(this,this.defensiveAction,[]));
+	var b = p.addButton("Stun",new db.link(this,this.addEffect,["stun",1]));
+	var b = p.addButton("Grapple");
+	var b = p.addButton("Prone",new db.link(this,this.addEffect,["prone",1]));
+	var b = p.addButton("K.O.");
+	var b = p.addButton("Bound/Helpless");
+	var b = p.addButton("Fight Defensively");
+	var b = p.addButton("Throwing Caution");
+	var b = p.addButton("Delay");
+	var b = p.addButton("Rush");
+	var b = p.addButton("Reset",new db.link(this,this.resetEffects,["all"]));
+
+	var p = this.ui.addPanel("Effects");
+	this.panels.effects = p;
+	this.refreshEffects();
 };
 
 GM.npcCombatSVC.prototype.initialize = function() {
