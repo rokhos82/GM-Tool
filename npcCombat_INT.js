5d5efe07f607b42ce6fd93564271824df23b641b
Justin Lane <justin.l.lane@gmail.com>
2013-01-23 13:14:52 -0700

    Imported npc card css and started work on generating
    the npc cards dynamically.

--- npcCombat_INT.js
+++ npcCombat_INT.js
@@ -1,8 +1,40 @@
 GM.npcCombatINT = function(svc) {
 	GM.debug.log("CALL: GM.npcCombatINT","Initializing NPC combat interface",2);
 	this.svc = svc;
-	this.ui = new ui.panel(this.svc.getName());
+	this.ui = new ui.panel();
+	this.ui.addClass("npc_card");
 	this.parent = null;
+
+	var p = this.ui.addPanel();
+	p.addClass("colset");
+	var s = p.addText("Name: " + this.svc.getName());
+	s.addClass("name");
+
+	var p = this.ui.addPanel();
+	p.addClass("colset");
+	var col = p.addPanel();
+	col.addClass("col");
+	var t = col.addTable();
+	t.addClass("small");
+
+	var attributes = this.svc.getData("attributes");
+	var attr_map = {
+		"STR":"strength",
+		"SIZ":"size",
+		"AGL":"agility",
+		"REF":"reflexes",
+		"CON":"constitution",
+		"FOR":"fortitude",
+		"REA":"reasoning",
+		"WIL":"willpower",
+		"SPR":"spirit",
+		"PER":"perception"
+	};
+
+	for(var a in attr_map) {
+		var attr = attr_map[a];
+		t.addRow([a,attributes[attr].score]);
+	};
 
 	GM.debug.log("END: GM.npcCombatINT","Finished initializing NPC combat interface",2);
 };
