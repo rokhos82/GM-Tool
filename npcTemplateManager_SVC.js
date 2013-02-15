091ad647cfb9959f417fea65d3f3788dbd6b39ce
Justin Lane <justin.l.lane@gmail.com>
2013-02-15 09:59:43 -0700

    Fleshed out the template manager service a bit more

--- npcTemplateManager_SVC.js
+++ npcTemplateManager_SVC.js
@@ -1,9 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // npcTemplateManagerSVC
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-GM.npcTemplateManagerSVC = function() {
+GM.npcTemplateManagerSVC = function(dat) {
 	GM.debug.log("BEGIN: GM.npcTemplateManagerSVC","Started constructing npcTemplateManagerSVC object",2);
 	this.parent = null;
+	this.dat = dat;
 	this.lists = {};
 	GM.debug.log("FINISH: GM.npcTemplateManagerSVC","Finished constructing npcTemplateManagerSVC object",2);
 };
@@ -11,7 +12,7 @@ GM.npcTemplateManagerSVC = function() {
 // -------------------------------------------------------------------------------------------------
 // getTemplates - returns a list of templates and a dictionary of the template objects.
 // -------------------------------------------------------------------------------------------------
-GM.npcTemplateManagerSVC.prototype.getTemplates = function() {
+GM.npcTemplateManagerSVC.prototype.getTemplateInfo = function() {
 	GM.debug.log("CALL: GM.npcTemplateManagerSVC.getTemplates","Retrieving template objects from data object",2);
 	if(!this.lists.templates)
 		this.buildLists();
@@ -24,6 +25,9 @@ GM.npcTemplateManagerSVC.prototype.getTemplates = function() {
 	return ret;
 };
 
+// -------------------------------------------------------------------------------------------------
+// 
+// -------------------------------------------------------------------------------------------------
 GM.npcTemplateManagerSVC.prototype.buildLists = function() {
 	GM.debug.log("CALL: GM.npcTemplateManagerSVC.buildLists","Building the lists from the data object",2);
 	// Build the list of custom npc templates
@@ -32,4 +36,35 @@ GM.npcTemplateManagerSVC.prototype.buildLists = function() {
 	for(var t in templates) {
 		this.lists.templates[t] = 1;
 	}
+};
+
+// -------------------------------------------------------------------------------------------------
+// 
+// -------------------------------------------------------------------------------------------------
+GM.npcTemplateManagerSVC.prototype.getTemplate = function(t) {
+	return this.dat.templates.npc[t];
+};
+
+// -------------------------------------------------------------------------------------------------
+// 
+// -------------------------------------------------------------------------------------------------
+GM.npcTemplateManagerSVC.prototype.newTemplate = function() {
+};
+
+// -------------------------------------------------------------------------------------------------
+// 
+// -------------------------------------------------------------------------------------------------
+GM.npcTemplateManagerSVC.prototype.copyTemplate = function() {
+};
+
+// -------------------------------------------------------------------------------------------------
+// 
+// -------------------------------------------------------------------------------------------------
+GM.npcTemplateManagerSVC.prototype.deleteTemplate = function() {
+};
+
+// -------------------------------------------------------------------------------------------------
+// 
+// -------------------------------------------------------------------------------------------------
+GM.npcTemplateManagerSVC.prototype.editTemplate = function() {
 };
