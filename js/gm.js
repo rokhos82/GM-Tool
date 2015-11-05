a34ca2cc303d64f6a47c68f3a39037c911cdfc54
Justin Lane <justin.l.lane@gmail.com>
2015-11-04 21:11:41 -0700

    20151104 2111 - Work on the dashboard

--- js/gm.js
+++ js/gm.js
@@ -16,8 +16,14 @@ gm.app = new angular.module("gm-tool",[]);
 gm.controllers = {};
 gm.controllers.main = gm.app.controller("mainCtl",["$scope",function($scope) {
     $scope.greeting = "Welcome to the GM-Tool!";
-    $scope.state = gm.states.npcs;
+    $scope.state = gm.states.dashboard;
     $scope.states = gm.states;
+}]);
+gm.controllers.dashboard = gm.app.controller("dashboardCtl",["$scope",function($scope) {
+    $scope.news = {
+        title: "Test News Alert",
+        text: "In the event of an actual news alert the text for said alert would be inserted here.  Since this is a test alert, there will not be much text here"
+    };
 }]);
 gm.controllers.npc = gm.app.controller("npcCtl",["$scope",function($scope) {
     $scope.list = {
